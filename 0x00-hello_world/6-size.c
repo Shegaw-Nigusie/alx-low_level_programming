f2fcdab1f0861d7544376281e00629a0158d1911
shegaw <86460301+shegaw2@users.noreply.github.com>
2022-07-01 14:01:16 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
+/*
+ * File: 6-size.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
-#include <conio.h>
-main(void)
+
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-printf("short int is %2d bytes \n", sizeof(short int));
-printf("int is %2d bytes \n", sizeof(int));
-printf("int * is %2d bytes \n", sizeof(int *));
-printf("long int is %2d bytes \n", sizeof(long int));
-printf("long int * is %2d bytes \n", sizeof(long int *));
-printf("signed int is %2d bytes \n", sizeof(signed int));
-printf("unsigned int is %2d bytes \n", sizeof(unsigned int));
-printf("float is %2d bytes \n", sizeof(float));
-printf("float * is %2d bytes \n", sizeof(float *));
-printf("double is %2d bytes \n", sizeof(double));
-printf("double * is %2d bytes \n", sizeof(double *));
-printf("long double is %2d bytes \n", sizeof(long double));
-printf("\n");
-printf("signed char is %2d bytes \n", sizeof(signed char));
-printf("char is %2d bytes \n", sizeof(char));
-printf("char * is %2d bytes \n", sizeof(char *));
-printf("unsigned char is %2d bytes \n", sizeof(unsigned char));
-return(0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
 }
