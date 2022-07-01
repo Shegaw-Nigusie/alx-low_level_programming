d01f60f07928cc9a30df9643fd495de832b0dfce
shegaw2 <shegaw21nigusie@gmail.com>
2022-07-01 11:28:35 +0000

    numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
 *main ->print whether the number stored in the variable n is positive or negative
 *based a condtion
-*alwways return true
+*Return->alwways  true
 */
 int main(void)
 {
@@ -18,15 +18,15 @@ scanf("%d", &n);
 
 if (n > 0)
 {
-printf("%d Number is POSITIVE\n", n);
+printf("%d Number is POSITIVE\n",n);
 }
 if (n < 0)
 {
-printf("%d Number is NEGATIVE\n", n);
+printf("%d Number is NEGATIVE\n",n);
 }
 if (n == 0)
 {
-printf("%d Number is ZERO\n", n);
+printf("%d Number is ZERO\n",n);
 }
 
 return (0);
