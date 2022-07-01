66c3a5d8ea63afad27f498e0ba4233ea889c42bd
shegaw2 <shegaw21nigusie@gmail.com>
2022-07-01 09:29:21 +0000

    n-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* Input number from user */
+printf("Enter any number: ");
+scanf("%d", &n);
+
+
+if(num > 0)
+{
+printf("%dNumber is POSITIVE\n", n);
+}
+if(num < 0)
+{
+printf("%dNumber is NEGATIVE\n", n);
+}
+if(num == 0)
+{
+printf("%d Number is ZERO\n", n);
+}
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this is README file   variable if else  while proramming assignment
