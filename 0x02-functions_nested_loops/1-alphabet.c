0e3ee5ebaace9baafc32c170acd0f11896c46546
shegaw2 <shegaw21nigusie@gmail.com>
2022-07-05 12:10:21 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,22 @@
  * File: 1-alphabet.c
  * Auth: shegaw2
  */
-
+#include "main.h"
 #include <stdio.h>
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * Rerurn: always 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
-
+		}
 	putchar('\n');
-
 	return (0);
 }
 
