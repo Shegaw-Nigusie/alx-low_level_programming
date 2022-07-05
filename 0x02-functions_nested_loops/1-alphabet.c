7377050c8f2ed253e0f0eebcdc9491782efd881e
shegaw <86460301+shegaw2@users.noreply.github.com>
2022-07-05 13:51:22 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,19 @@
-/**
+/*
  * File: 1-alphabet.c
  * Auth: shegaw2
  */
+
 #include "main.h"
+
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- * Rerurn: always 0
  */
 void print_alphabet(void)
 {
-	char c;
+	char letter;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
-
-	return (0);
 }
-
