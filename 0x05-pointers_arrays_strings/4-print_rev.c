5227d909925c555adebadce9b94ade23f2dc6aa0
shegaw <86460301+shegaw2@users.noreply.github.com>
2022-07-11 13:27:34 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -21,3 +21,22 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[2][100];
+    printf("Type Text: ");
+
+    scanf("%[^\n]%*c", str[0]);
+    int length = strlen(str[0]);
+    int i, j;
+    for (i = 0, j = length - 1; i < length; i++, j--)
+    {
+        str[1][i] = str[0][j];
+    }
+    printf("Original Word: %s\n", str[0]);
+    printf("Reverse word: %s\n", str[1]);
+    return 0;
+}
