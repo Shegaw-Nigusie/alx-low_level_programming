ae1c4824ed66d162714b1e08c7babc2b9cba2ad4
shegaw <86460301+shegaw2@users.noreply.github.com>
2022-07-28 13:07:03 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,7 +3,7 @@
 
 /**
  * File: main.h
- * Auth: Brennan D Baraban
+ * Auth: s
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0B-more_malloc_free directory.
  */
